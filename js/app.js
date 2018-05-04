1b42b5d6b3f6ff1060a9fab5ab0cd5a9cce95265
v.rodriguez1425@gmail.com <v.rodriguez1425@gmail.com>
2018-05-04 02:00:57 -0300

    agrego section worj y theteam con modal en img

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+ /** para abrir el modal */
+
+const modal = document.getElementById("myModal");
+const img = document.getElementById("imgModal");
+const modalimg= document.getElementsByClassName('img01');
+
+ /** creo la variable para el boton cerrar del modal */
+
+ const btnCerrar = document.getElementById("close");
+
+ /**Listeners */
+
+ /**Funcion para poder mostrar el modal al hacer click */
+img.onclick = function(){
+    modal.style.display = "block";
+    modalimg.src = this.src;
+}
+
+ /**Funcion que me cierre el modal */
+
+ btnCerrar.onclick = function() {
+   modal.style.display = "none";
+ };
